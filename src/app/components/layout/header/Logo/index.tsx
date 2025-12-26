f79ac5d0dce89ceacbe9c89bb257db860ff383e2
Shubham <info.shubhamanchliya@gmail.com>
2025-12-26 21:38:28 +0530

    added logo

--- src/app/components/layout/header/Logo/index.tsx
+++ src/app/components/layout/header/Logo/index.tsx
@@ -1,31 +1,33 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
-interface HeaderProps { }
+interface HeaderProps {}
 const Logo: React.FC<HeaderProps> = () => {
-    return (
-        <Link href="/">
-            <Image
-                src="/images/logo/logo.svg"
-                alt="logo"
-                width={117}
-                height={34}
-                style={{ width: 'auto', height: 'auto' }}
-                quality={100}
-                priority={true}
-                className='dark:hidden'
-            />
-            <Image
-                src="/images/logo/DarkModeLogo.svg"
-                alt="logo"
-                width={160}
-                height={50}
-                style={{ width: 'auto', height: 'auto' }}
-                quality={100}
-                className='dark:block hidden'
-            />
-        </Link>
-    );
+  return (
+    <Link href="/">
+      <Image
+        src="/images/logo/sms21221.png"
+        alt="logo"
+        width={110}
+        height={34}
+        style={{ width: "10rem", height: "5rem", marginLeft: "2rem" }}
+        quality={100}
+        priority={true}
+        className="dark:hidden"
+      />
+      <Image
+        // src="/images/logo/DarkModeLogo.svg"
+        src="/images/logo/sms21221.png"
+        alt="logo"
+        width={160}
+        height={50}
+        // style={{ width: "auto", height: "auto" }}
+        style={{ width: "10rem", height: "5rem", marginLeft: "2rem" }}
+        quality={100}
+        className="dark:block hidden"
+      />
+    </Link>
+  );
 };
 
 export default Logo;
